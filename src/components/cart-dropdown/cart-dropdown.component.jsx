6aa26787d1285e0c701bf8ec085a6aba96d1b4cf
Jason Shaw <clients@uptocodetech.com>
2022-06-13 16:27:20 -0400

    implement dynamic checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,6 @@
 import {useContext} from "react";
 import {CartContext} from "../../contexts/cart.context";
+import {useNavigate} from "react-router-dom";
 
 import './cart-dropdown.styles.scss';
 import Button from "../button/button.component";
@@ -7,6 +8,11 @@ import CartItem from "../cart-item/cart-item.component";
 
 const CartDropDown = () => {
 	const {cartItems} = useContext(CartContext);
+	const navigate = useNavigate();
+
+	const goToCheckoutHandler = () => {
+		navigate('/checkout');
+	}
 
 	return (
 		<div className='cart-dropdown-container'>
@@ -15,7 +21,7 @@ const CartDropDown = () => {
 					<CartItem key={item.id} cartItem={item}/>
 				))}
 			</div>
-			<Button>Go To Checkout</Button>
+			<Button onClick={goToCheckoutHandler}>Go To Checkout</Button>
 		</div>
 	);
 }
